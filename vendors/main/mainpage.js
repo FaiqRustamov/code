5fb8001dd611a36688fcb1b2d5b8a04421562d63
bdksdk <leman.ibrahimli23@gmail.com>
2020-12-20 22:12:46 +0400

    all

--- vendors/main/mainpage.js
+++ vendors/main/mainpage.js
@@ -10,7 +10,57 @@ $(document).ready(function () {
       // const element = document.querySelector('#datacourse');
       // const element2 = document.querySelector('#datacorse2');
       // const element3 = document.querySelector('#datacurse3');
-      console.log(data);
+      const elementdata=document.querySelector('#Feature_id');
+      console.log(data[0]);
+
+
+
+        elementdata.innerHTML=`
+      <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="single_feature_2">
+          <center>
+            <img class="img-fluid" style="padding" src="${data[0].photo}" alt="" />
+          </center>
+          <a href="course-details.html">
+          <div class="desc">
+          <h2 class="card-title corse_h2">${data[0].name}</h2>
+          <p class="card-text corse_p">${data[0].description}</p>
+          </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="single_feature_2">
+          <center>
+            <img class="img-fluid" src="${data[0].photo}" alt="" />
+          </center>
+          <a href="">
+          <div class="desc">
+          <h2 class="card-title corse_h2">${data[1].name}</h2>
+          <p class="card-text corse_p">${data[1].description}</p>
+          </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="single_feature_2">
+          <center>
+            <img class="img-fluid" src="" alt="" />
+          </center>
+          <a href="">
+          <div class="desc">
+          <h2 class="card-title corse_h2"></h2>
+          <p class="card-text corse_p"></p>
+          </div>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  `
       // element.innerHTML = `${data[0].title}`
       // element2.innerHTML = `${data[0].topic.name}`
       // element3.innerHTML = `${data[0].text}`
